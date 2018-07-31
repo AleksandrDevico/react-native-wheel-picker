8fc9e0f26b149f7757a27e560e9afa05bf17fd61
Aleksandr <aleksandr@devico.io>
2018-07-31 17:05:15 +0300

    Changed code structure

---
+++ android/src/main/java/com/wheelpicker/WheelPicker.java
@@ -0,0 +1,32 @@
+package com.wheelpicker;
+
+import com.facebook.react.ReactPackage;
+import com.facebook.react.bridge.JavaScriptModule;
+import com.facebook.react.bridge.NativeModule;
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.uimanager.ViewManager;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Arrays;
+import java.util.ArrayList;
+
+public class WheelPicker implements ReactPackage {
+    @Override
+    public List<NativeModule> createNativeModules(ReactApplicationContext reactContext) {
+        List<NativeModule> modules = new ArrayList<>();
+        return modules;
+    }
+
+    @Override
+    public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
+        return Arrays.<ViewManager>asList(
+            new WheelPickerManager()
+        );
+    }
+
+    public List<Class<? extends JavaScriptModule>> createJSModules() {
+        return Collections.emptyList();
+    }
+
+}
---
+++ index.d.ts
@@ -0,0 +1,27 @@
+declare module "react-native-wheel-picker" {
+  import * as React from "react"
+  import { StyleProp, NativeSyntheticEvent, TextStyle, ViewStyle } from "react-native"
+
+  export interface WheelPickerProps {
+    itemStyle?: StyleProp<TextStyle>,
+    onValueChange?: (arg: string | number) => void,
+    selectedValue?: string | number,
+    style?: StyleProp<ViewStyle>,
+  }
+
+  export type State = {
+    selectedItemPosition: number
+  }
+
+  export interface WheelPickerItemProps {
+    label: string,
+    value: string | number,
+  }
+
+  export class WheelPickerItem extends React.Component<WheelPickerItemProps> {}
+
+  export default class WheelPicker extends React.Component<WheelPickerProps, State> {
+    static Item: typeof WheelPickerItem
+  }
+
+}
